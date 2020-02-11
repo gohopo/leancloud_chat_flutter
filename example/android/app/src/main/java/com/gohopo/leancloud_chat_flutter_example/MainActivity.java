1adca0105f6c911b4a6b7496c8495dd406e70585
gohopo <gohopo@live.com>
2020-02-11 16:44:46 +0800

    使用ObjectiveC和Java -i objc -a java

---
+++ example/android/app/src/main/java/com/gohopo/leancloud_chat_flutter_example/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gohopo.leancloud_chat_flutter_example;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
--- ios/Classes/LeancloudChatFlutterPlugin.m
+++ ios/Classes/LeancloudChatFlutterPlugin.m
@@ -1,15 +1,20 @@
 #import "LeancloudChatFlutterPlugin.h"
-#if __has_include(<leancloud_chat_flutter/leancloud_chat_flutter-Swift.h>)
-#import <leancloud_chat_flutter/leancloud_chat_flutter-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "leancloud_chat_flutter-Swift.h"
-#endif
 
 @implementation LeancloudChatFlutterPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftLeancloudChatFlutterPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"leancloud_chat_flutter"
+            binaryMessenger:[registrar messenger]];
+  LeancloudChatFlutterPlugin* instance = [[LeancloudChatFlutterPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
